4799f93315b6cf22f317bd731aebacda12a42d48
César García Bautista <cesar.inmatmex.oficina@gmail.com>
2026-01-03 16:11:44 +0000

    cambia el h de codigo de barra 1 y 2 a h:20

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,14 +58,14 @@ const TRY_PAGE_RECTANGLES_DEFAULT: Omit<Rectangle, 'id'>[] = [
     { label: "FECHA ENTREGA", x: 193, y: 311, width: 238, height: 33 },
     { label: "CANTIDAD", x: 69, y: 96, width: 50, height: 69 },
     { label: "CLIENTE INFO", x: 48, y: 900, width: 291, height: 155 },
-    { label: "CODIGO DE BARRA", x: 144, y: 445, width: 154, height: 30 },
+    { label: "CODIGO DE BARRA", x: 144, y: 445, width: 154, height: 20 },
     { label: "NUM DE VENTA", x: 53, y: 51, width: 168, height: 25 },
     { label: "PRODUCTO", x: 156, y: 88, width: 269, height: 60 },
     // Segunda etiqueta
     { label: "FECHA ENTREGA 2", x: 586, y: 311, width: 238, height: 33 },
     { label: "CANTIDAD 2", x: 462, y: 96, width: 50, height: 69 },
     { label: "CLIENTE INFO 2", x: 441, y: 900, width: 291, height: 155 },
-    { label: "CODIGO DE BARRA 2", x: 537, y: 445, width: 154, height: 30 },
+    { label: "CODIGO DE BARRA 2", x: 537, y: 445, width: 154, height: 20 },
     { label: "NUM DE VENTA 2", x: 446, y: 51, width: 168, height: 25 },
     { label: "PRODUCTO 2", x: 549, y: 88, width: 269, height: 60 },
     // Tercer juego de coordenadas (fallback)
@@ -1474,5 +1474,7 @@ export default function TryPage() {
     </main>
   );
 }
+
+    
 
     
